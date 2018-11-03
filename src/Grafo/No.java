65f2eb126326454a68d7e36f5b2836b63265aa7c
FlowOverStack <thundertufik@gmail.com>
2018-11-03 17:14:46 -0300

    Atualizada Descoberta de Fortemente Conexos.

--- src/Grafo/No.java
+++ src/Grafo/No.java
@@ -4,46 +4,96 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class No<E> {
-
-    ArrayList<Conexao<E>> ligadoA;
-    public E valor;
-    public int grauDeEntrada;
+    ArrayList<Conexao<E>> entrada;
+    ArrayList<Conexao<E>> saida;
+    E valor;
+    int grauDeEntrada;
+    int grauDeSaida;
     boolean visitado;
 
     public No(E valor) {
-        ligadoA = new ArrayList<>();
+        this.entrada =   new ArrayList<>();
+        this.saida =     new ArrayList<>();
         this.valor = valor;
         this.grauDeEntrada = 0;
+        this.grauDeSaida = 0;
     }
 
     public void conectarA(No<E> no) {
+        grauDeSaida++;
         no.grauDeEntrada++;
-        ligadoA.add(new Conexao<E>(no));
+        saida.add(new Conexao<>(no));
+        no.entrada.add(new Conexao<>(this));
     }
 
     public void conectarEspelho(No<E> no) {
+        grauDeSaida++;
         no.grauDeEntrada++;
         conectarA(no);
         no.conectarA(this);
     }
 
     public void conectarA(No<E> no, double valor) {
+        grauDeSaida++;
         no.grauDeEntrada++;
-        ligadoA.add(new Conexao<E>(no, valor));
+        saida.add(new Conexao<>(no, valor));
+        no.entrada.add(new Conexao<>(this, valor));
     }
 
     public void conectarEspelho(No<E> no, double valor) {
-        grauDeEntrada++;
+        grauDeSaida++;
         no.grauDeEntrada++;
         conectarA(no, valor);
         no.conectarA(this, valor);
     }
     
     void resetarGrau () {
-        grauDeEntrada = ligadoA.size();
+        grauDeSaida = entrada.size();
+        grauDeEntrada = saida.size();
     }
 
     public void remover(No<E> no) {
-        ligadoA.removeAll(Collections.singleton(new Conexao<E>(no)));
+        saida.removeAll(Collections.singleton(new Conexao<>(no)));
     }
+
+    public ArrayList<Conexao<E>> getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(ArrayList<Conexao<E>> entrada) {
+        this.entrada = entrada;
+    }
+
+    public ArrayList<Conexao<E>> getSaida() {
+        return saida;
+    }
+
+    public void setSaida(ArrayList<Conexao<E>> saida) {
+        this.saida = saida;
+    }
+
+    public E getValor() {
+        return valor;
+    }
+
+    public void setValor(E valor) {
+        this.valor = valor;
+    }
+
+    public int getGrauDeEntrada() {
+        return grauDeEntrada;
+    }
+    
+    public int getGrauDeSaida() {
+        return grauDeSaida;
+    }
+
+    public boolean isVisitado() {
+        return visitado;
+    }
+
+    public void setVisitado(boolean visitado) {
+        this.visitado = visitado;
+    }
+
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,22 +8,31 @@ public class Main {
     public static void main(String[] args) {
         Grafo<String> grafo = new Grafo<>();
         
-        No<String> A = new No<String>("A");
-        No<String> B = new No<String>("B");
-        No<String> C = new No<String>("C");
+        No<String> A = new No<>("A");
+        No<String> B = new No<>("B");
+        No<String> C = new No<>("C");
         
         grafo.inserirNo(A);
         grafo.inserirNo(B);
         grafo.inserirNo(C);
         
         A.conectarA(B);
-        A.conectarA(C);
-        B.conectarA(C);
+        B.conectarA(A);
+        C.conectarA(A);
         
         ArrayList<No> nos = grafo.ordenacaoTopologica();
         
         for (No<String> no: nos) {
-            System.out.println(no.valor);
+            System.out.println(no.getValor());
+        }
+        
+        ArrayList<ArrayList<No<String>>> cmpn = grafo.fortementeConexos();
+        
+        for (ArrayList<No<String>> lista: cmpn) {
+            System.out.println("Lista:");
+            for (No<String> no: lista) {
+                System.out.println(no.getValor());
+            }
         }
     }
 }
