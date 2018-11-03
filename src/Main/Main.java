9a4a01ca433afd671bb60a6c8eb82d50c2f526d7
FlowOverStack <thundertufik@gmail.com>
2018-11-03 00:41:10 -0300

    Consertada a Ordenação Topológica.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,29 @@
 package Main;
 
+import Grafo.Grafo;
+import Grafo.No;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        Grafo<String> grafo = new Grafo<>();
         
+        No<String> A = new No<String>("A");
+        No<String> B = new No<String>("B");
+        No<String> C = new No<String>("C");
+        
+        grafo.inserirNo(A);
+        grafo.inserirNo(B);
+        grafo.inserirNo(C);
+        
+        A.conectarA(B);
+        A.conectarA(C);
+        B.conectarA(C);
+        
+        ArrayList<No> nos = grafo.ordenacaoTopologica();
+        
+        for (No<String> no: nos) {
+            System.out.println(no.valor);
+        }
     }
 }
